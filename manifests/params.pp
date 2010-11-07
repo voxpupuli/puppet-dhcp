a24944d3a01a2d4a7ec0bd8c5661715df11496d0
Zach Leslie <zach.leslie@gmail.com>
2010-11-07 15:09:56 -0800

    add secondary nameserver

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class dhcp::params {
     }
 
     $domain = 'znet.local'
-    $nameservers = '10.210.18.51'
+    $nameservers = '10.210.18.50, 10.210.18.51'
     $ntpserver = '10.210.18.253'
     $pxeserver = '10.210.18.50'
     $filename = 'pxelinux.0'
