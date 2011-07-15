297f463a688208683850a0552e4f5f9626d6777a
Ben Hughes <git@mumble.org.uk>
2011-07-15 11:42:22 -0700

    tidy up quoting of things so dhcpd doesn't complain. Add /etc/default/ file for debian/ubuntu to pick interface

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,8 @@ class dhcp(
   $dnsdomain,
   $nameservers,
   $ntpservers,
+  $interfaces = undef,
+  $interface = "NOTSET",
   $dnsupdatekey = undef,
   $pxeserver = undef,
   $pxefilename = undef,
@@ -13,6 +15,17 @@ class dhcp(
   $dhcp_dir    = $dhcp::params::dhcp_dir
   $packagename = $dhcp::params::packagename
   $servicename = $dhcp::params::servicename
+
+  # Incase people set interface instead of interfaces work around
+  # that. If they set both, use interfaces and the user is a unwise
+  # and deserves what they get.
+  if $interface != "NOTSET" and $interfaces == undef {
+    $dhcp_interfaces = [ $interface ]
+  } elsif $interface == "NOTSET" and $interfaces == undef {
+    fail( "You need to set \$interfaces in $module_name")
+  } else {
+    $dhcp_interfaces = $interfaces
+  }
 
   package {
     "$packagename":
@@ -28,9 +41,25 @@ class dhcp(
       owner   => root,
       group   => 0,
       mode    => 644,
-      require => Package["$packagename"],
+      require => Package[$packagename],
       content => template("dhcp/dhcpd.conf.erb");
   }
+
+  # Only debian and ubuntu have this style of defaults for startup.
+  case $operatingsystem {
+    'debian','ubuntu': {
+      file{ '/etc/default/isc-dhcp-server':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        before  => Package[$packagename],
+        notify  => Service[$servicename],
+        content => template('dhcp/debian/default_isc-dhcp-server'),
+      }
+    }
+  }
+
 
   include concat::setup
   concat { "${dhcp_dir}/dhcpd.pools": }
