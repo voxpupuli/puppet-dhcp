a1d7dded3c490ed2a6aae5b157612de5668132da
Zach Leslie <zach@puppetlabs.com>
2012-01-09 17:41:05 -0800

    update tests to be current

--- tests/init.pp
+++ tests/init.pp
@@ -1,13 +1,29 @@
-dhcp::pool {
-  "supernet.lan":
-    network  => "192.168.1.0",
-    mask     => "255.255.255.0",
-    range    => "192.168.1.150 192.168.1.199",
-    gateway  => "192.168.1.1";
+$ddnskeyname = 'dhcp_updater'
+
+class { 'dhcp':
+  dnsdomain    => [
+    'example.com',
+    '1.1.10.in-addr.arpa',
+    ],
+  nameservers  => ['10.1.1.10'],
+  ntpservers   => ['us.pool.ntp.org'],
+  interfaces   => ['eth0'],
+  dnsupdatekey => "/etc/bind/keys.d/$ddnskeyname",
+  require      => Bind::Key[ $ddnskeyname ],
+  pxeserver    => '10.1.1.5',
+  pxefilename  => 'pxelinux.0',
+}
+
+dhcp::pool{ 'example.com':
+  network => '10.1.1.0',
+  mask    => '255.255.255.0',
+  range   => '10.1.1.100 10.1.1.200',
+  gateway => '10.1.1.1',
 }
 
 dhcp::host {
-  'server1.supernet.lan':
-    mac => "00:00:00:00:00:00",
-    ip  => "192.168.1.10";
+  'gateway':
+    mac => "00:11:22:33:44:55",
+    ip  => "10.1.1.1",
 }
+
