3e04bc13a4ffc4bce5efd41cd459ad3bbfbb7efa
Zach Leslie <zach@puppetlabs.com>
2012-04-06 10:59:08 -0700

    Merge commit 'fe12b3d6aee1f4890668a5bd2e43a1589e176c93' into dhcp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,26 @@
 class dhcp::params {
 
-    $dhcp_dir = $operatingsystem ? {
-        debian  => "/etc/dhcp",
-        ubuntu  => "/etc/dhcp3",
-        darwin  => "/opt/local/etc/dhcp",
-        default => "/etc",
+  case $operatingsystem {
+    'debian': {
+      $dhcp_dir    = "/etc/dhcp"
+      $packagename = "isc-dhcp-server"
+      $servicename = "isc-dhcp-server"
     }
+    'ubuntu': {
+      $dhcp_dir    = "/etc/dhcp3"
+      $packagename = "isc-dhcp-server"
+      $servicename = "isc-dhcp-server"
+    }
+    'darwin': {
+      $dhcp_dir    = "/opt/local/etc/dhcp"
+      $packagename = "dhcp"
+      $servicename = "org.macports.dhcpd"
+    }
+    'freebsd': {
+      $dhcp_dir    = "/usr/local/etc"
+      $packagename = "net/isc-dhcp42-server"
+      $servicename = "isc-dhcpd"
+    }
+  }
 
-    $packagename = $operatingsystem ? {
-      darwin  => "dhcp",
-      default => "isc-dhcp-server",
-    }
-
-    $servicename = $operatingsystem ? {
-      darwin  => "org.macports.dhcpd",
-      default => "isc-dhcp-server",
-    }
 }
