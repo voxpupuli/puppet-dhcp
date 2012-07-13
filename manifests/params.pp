bfee29516f8b51eab60a5bdfaa0e8626f39dbf34
crayfishx <craig@craigdunn.org>
2012-07-13 14:34:14 -0700

    Removed unwanted blank line

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,7 +25,6 @@ class dhcp::params {
       $packagename = 'net/isc-dhcp42-server'
       $servicename = 'isc-dhcpd'
     }
-
     'redhat','fedora','centos': {
       $dhcp_dir    = '/etc/dhcp'
       $packagename = 'dhcp'
