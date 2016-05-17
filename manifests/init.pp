cd8f6c49e23f0c26a7a2cfd5742bb09cfa102e58
Tim Meusel <tim@bastelfreak.de>
2016-05-17 14:08:44 +0200

    use correct quotes for concat order
    these are ints, but we have to provide them as strings :sadface:

--- manifests/init.pp
+++ manifests/init.pp
@@ -196,7 +196,7 @@ class dhcp (
   concat::fragment { 'dhcp-ignoredsubnets-header':
     target  => "${dhcp_dir}/dhcpd.ignoredsubnets",
     content => "# DHCP Subnets (ignored)\n",
-    order   => 01,
+    order   => '01',
   }
 
   # dhcpd.hosts
