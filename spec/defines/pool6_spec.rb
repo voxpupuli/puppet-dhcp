d3ea448b6df8febe3bd65a885034943c5db569f4
Alexander Lindquister <alelindq@cisco.com>
2016-10-21 09:50:27 +0200

    added dhcp::pool6

---
+++ spec/defines/pool6_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'dhcp::pool6', type: :define do
+  let :title do
+    'test_pool6'
+  end
+  let(:facts) do
+    {
+      concat_basedir: '/dne',
+      osfamily: 'RedHat'
+    }
+  end
+  let :params do
+    {
+      'network'  => '2001:db8::',
+      'prefix'   => '64',
+      'range'    => '2001:db8::100 2001:db8::110'
+    }
+  end
+
+  it { should contain_concat__fragment("dhcp_pool_#{title}") }
+end
