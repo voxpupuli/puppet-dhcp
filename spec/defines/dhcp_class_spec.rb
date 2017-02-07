dc088db771952c4e58e98e748fde1e9768201805
Dominic Cleal <dominic@cleal.org>
2017-02-07 20:35:34 +0100

    Add dhcp_class define to create custom DHCP class
    Fixes #86

---
+++ spec/defines/dhcp_class_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe 'dhcp::dhcp_class', type: :define do
+  let(:title) { 'test_class' }
+  let(:header) { ['#################################', "# Custom class #{title}", '#################################'] }
+  let(:facts) do
+    {
+      osfamily: 'RedHat'
+    }
+  end
+
+  context 'with parameters string' do
+    let(:params) do
+      { 'parameters' => 'match option vendor-class-identifier' }
+    end
+
+    it { is_expected.to contain_concat__fragment("dhcp_class_#{title}") }
+
+    it 'creates a class declaration' do
+      content = catalogue.resource('concat::fragment', "dhcp_class_#{title}").send(:parameters)[:content]
+      expected_lines = header + [
+        %(class "#{title}" \{),
+        '  match option vendor-class-identifier;',
+        '}'
+      ]
+      expect(content.split("\n")).to eq(expected_lines)
+    end
+  end
+
+  context 'with parameters array' do
+    let(:params) do
+      { 'parameters' => ['match option vendor-class-identifier', 'match option identifier-2'] }
+    end
+
+    it { is_expected.to contain_concat__fragment("dhcp_class_#{title}") }
+
+    it 'creates a class declaration' do
+      content = catalogue.resource('concat::fragment', "dhcp_class_#{title}").send(:parameters)[:content]
+      expected_lines = header + [
+        %(class "#{title}" \{),
+        '  match option vendor-class-identifier;',
+        '  match option identifier-2;',
+        '}'
+      ]
+      expect(content.split("\n")).to eq(expected_lines)
+    end
+  end
+end
