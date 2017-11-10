99536ef3566b785087ea5a6b7ef5362e04e33f42
aquister <aquister@users.noreply.github.com>
2017-11-10 11:32:51 +0100

    dhcp::failover parameter scope fix (#165)
    Remove inheritance of dhcp::params

--- manifests/failover.pp
+++ manifests/failover.pp
@@ -11,9 +11,9 @@ class dhcp::failover (
   $load_split           = '128',
   $load_balance         = '3',
   $omapi_key            = '',
-  $dhcp_dir             = $dhcp::params::dhcp_dir,
-  $dhcpd_conf_filename  = $dhcp::params::dhcpd_conf_filename,
-) inherits dhcp::params {
+  $dhcp_dir             = $dhcp::dhcp_dir,
+  $dhcpd_conf_filename  = $dhcp::dhcpd_conf_filename,
+) {
 
   concat::fragment { 'dhcp-conf-failover':
     target  => "${dhcp_dir}/${dhcpd_conf_filename}",
