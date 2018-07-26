f759eb53df01f4bcc369e4bd116cdb68cef6201b
Jan Vansteenkiste <jan@vstone.eu>
2018-07-26 10:31:19 +0200

    Debian default ipv4 interfaces config has changed
    Since stretch, the /etc/default/isc-dhcp-server entry for the
    interfaces to listen on has changed to INTERFACESv4. This change
    reflects that.
    
    Fixes #177 and closes #185

--- spec/classes/dhcp_spec.rb
+++ spec/classes/dhcp_spec.rb
@@ -469,9 +469,17 @@ describe 'dhcp', type: :class do
       }
     end
 
-    context 'Debian' do
+    context 'Debian < 9' do
       let :facts do
-        default_facts.merge(operatingsystem: 'Debian', os: { family: 'Debian' })
+        default_facts.merge(
+          os: {
+            name: 'Debian',
+            family: 'Debian',
+            release: {
+              major: '8'
+            }
+          }
+        )
       end
       let :params do
         default_params.merge(interface: 'eth0')
@@ -482,6 +490,29 @@ describe 'dhcp', type: :class do
       it do
         is_expected.to contain_file('/etc/default/isc-dhcp-server'). \
           with_content(%r{INTERFACES=\"eth0\"})
+      end
+    end
+    context 'Debian > 9' do
+      let :facts do
+        default_facts.merge(
+          os: {
+            name: 'Debian',
+            family: 'Debian',
+            release: {
+              major: '9'
+            }
+          }
+        )
+      end
+      let :params do
+        default_params.merge(interface: 'eth0')
+      end
+
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to contain_package('isc-dhcp-server') }
+      it do
+        is_expected.to contain_file('/etc/default/isc-dhcp-server'). \
+          with_content(%r{INTERFACESv4=\"eth0\"})
       end
     end
     context 'Ubuntu' do
