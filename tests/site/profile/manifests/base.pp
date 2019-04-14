4906f4fccb6141f02b4cc7685b8770a917352e0a
nigemar <nige.marshall@gmail.com>
2019-04-14 18:22:38 +1000

    removed excess config

--- tests/site/profile/manifests/base.pp
+++ tests/site/profile/manifests/base.pp
@@ -3,10 +3,4 @@ class profile::base {
   #class { '::ntp': }
   notify { 'Inside the base class' : }
 
-  file { '/tmp/nmm.junk' :
-    ensure => 'present',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0640',
-  }
 }
