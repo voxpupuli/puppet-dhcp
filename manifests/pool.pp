aa0eabf3747a005c3e25f4ef4df0681da98bd1d8
Dan West <dwest@galois.com>
2019-07-17 12:23:14 -0700

    Adding a max-lease-time parameter to the pool scope.  Allow `fqdn` as a valid value in the `fixed-address` field.

--- manifests/pool.pp
+++ manifests/pool.pp
@@ -14,6 +14,7 @@ define dhcp::pool (
   Optional[Integer] $mtu                    = undef,
   String $domain_name                       = '',
   $ignore_unknown                           = undef,
+  Optional[Integer] $max_lease_time         = undef,
 ) {
   include dhcp::params
 
