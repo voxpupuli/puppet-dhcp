voxpupuli/puppet-dhcp

commit e303edbd0256228338b414350b313c5a3cdc3bcb

Fix CI and improve accepted data types

manifests/pool.pp
@@ -68,17 +68,17 @@ define dhcp::pool (
   Stdlib::IP::Address::V4 $network,
   Stdlib::IP::Address::V4 $mask,
   Stdlib::IP::Address::V4 $host_mask        = $mask,
-  $gateway                                  = '',
-  $range                                    = '',
-  $failover                                 = '',
-  $options                                  = '',
-  $parameters                               = '',
+  Optional[String[1]] $gateway              = undef,
+  Optional[Variant[Array[String[1],1],String[1]]] $range = undef,
+  Optional[String[1]] $failover             = undef,
+  Optional[String[1]] $options              = undef,
+  Optional[String[1]] $parameters           = undef,
   Optional[String[1]] $sharednetwork        = undef,
   Optional[Array[String]] $nameservers      = undef,
   Optional[Array[String]] $nameservers_ipv6 = undef,
   Optional[String] $pxeserver               = undef,
   Optional[Integer] $mtu                    = undef,
-  String $domain_name                       = '',
+  Optional[String[1]] $domain_name          = undef,
   $ignore_unknown                           = undef,
   Array[String[1]] $on_commit               = [],
   Array[String[1]] $on_release              = [],

manifests/pool6.pp
@@ -3,17 +3,17 @@
 define dhcp::pool6 (
   Stdlib::IP::Address::V6 $network,
   Integer $prefix,
-  String $range                             = '',
-  String $range_temp                        = '',
-  String $failover                          = '',
-  String $options                           = '',
-  String $parameters                        = '',
+  Optional[Variant[Array[String[1],1],String[1]]] $range = undef,
+  Optional[String[1]] $range_temp           = undef,
+  Optional[String[1]] $failover             = undef,
+  Optional[String[1]] $options              = undef,
+  Optional[String[1]] $parameters           = undef,
   Optional[String[1]] $sharednetwork        = undef,
   Optional[Array[String]] $nameservers      = undef,
   Optional[Array[String]] $nameservers_ipv6 = undef,
   Optional[String] $pxeserver               = undef,
   Optional[Integer] $mtu                    = undef,
-  String $domain_name                       = '',
+  Optional[String[1]] $domain_name          = undef,
   $ignore_unknown                           = undef,
   Array[String[1]] $on_commit               = [],
   Array[String[1]] $on_release              = [],

manifests/failover.pp
@@ -10,7 +10,7 @@ class dhcp::failover (
   $mclt                 = '300',
   $load_split           = '128',
   $load_balance         = '3',
-  $omapi_key            = '',
+  Optional[String[1]] $omapi_key = undef,
   $dhcp_dir             = $dhcp::dhcp_dir,
   $dhcpd_conf_filename  = $dhcp::dhcpd_conf_filename,
 ) {

manifests/host.pp
@@ -48,7 +48,7 @@ define dhcp::host (
   Dhcp::Mac $mac,
   String $ddns_hostname                 = $name,
   Hash $options                         = {},
-  String $comment                       = '',
+  Optional[String[1]] $comment          = undef,
   Boolean $ignored                      = false,
   Optional[Integer] $default_lease_time = undef,
   Optional[Integer] $max_lease_time     = undef,

manifests/init.pp
@@ -29,7 +29,7 @@ class dhcp (
   Integer[-1] $default_lease_time                                  = 43200,
   Integer[-1] $max_lease_time                                      = 86400,
   Stdlib::Ensure::Service $service_ensure                          = 'running',
-  Variant[String,Array[String[1]]] $globaloptions                  = '',
+  Optional[Variant[String,Array[String[1]]]] $globaloptions        = undef,
   Optional[Stdlib::Port] $omapi_port                               = undef,
   Optional[String[1]] $omapi_name                                  = undef,
   String[1] $omapi_algorithm                                       = 'HMAC-MD5',
@@ -46,7 +46,7 @@ class dhcp (
   Stdlib::Port $ldap_port                                          = 389,
   String[1] $ldap_server                                           = 'localhost',
   String[1] $ldap_username                                         = 'cn=root, dc=example, dc=com',
-  String $ldap_password                                            = '',
+  Optional[String[1]] $ldap_password                               = undef,
   String[1] $ldap_base_dn                                          = 'dc=example, dc=com',
   Enum['dynamic', 'static'] $ldap_method                           = 'dynamic',
   Optional[Stdlib::Absolutepath] $ldap_debug_file                  = undef,
@@ -319,16 +319,16 @@ class dhcp (
 
   # check if this is really a bool
   if $use_ldap {
-    if ($ldap_password == '') {
+    unless $ldap_password {
       fail('you must set $ldap_password')
     }
-    if ($ldap_server == '') {
+    unless $ldap_server {
       fail('you must set $ldap_server')
     }
-    if ($ldap_username == '') {
+    unless $ldap_username {
       fail('you must set $ldap_username')
     }
-    if ($ldap_base_dn == '') {
+    unless $ldap_base_dn {
       fail('you must set $ldap_username')
     }
     concat::fragment { 'dhcp-conf-ldap':

spec/classes/dhcp_spec.rb
@@ -26,6 +26,8 @@ describe 'dhcp', type: :class do
           name: 'RedHat',
           release: { major: '6' }
         },
+        osfamily: 'RedHat',
+        operatingsystem: 'RedHat',
         concat_basedir: '/dne'
       }
     end

templates/dhcpd.conf-header.erb
@@ -41,7 +41,7 @@ option <%= @option_code150_label -%> code 150 = <%= @option_code150_value -%>;
 <%   @globaloptions.each do |opt| -%>
 option <%= opt %>;
 <%   end -%>
-<% elsif @globaloptions != '' -%>
+<% elsif @globaloptions -%>
 option <%= @globaloptions %>;
 <% end -%>
 <% if has_variable?( 'mtu' ) && @mtu -%>

templates/dhcpd.host.erb
@@ -1,5 +1,5 @@
 host <%= @host %> {
-<% if not @comment.empty? -%>
+<% if @comment -%>
   # <%= @comment %>
 <% end -%>
   hardware ethernet   <%= @mac.upcase %>;

commit b5409e0df85a020b8523b00580b600a2ffbe6095

Fix the type of options and parameters in pool manifest
Fixes: e303edbd0256228338b414350b313c5a3cdc3bcb

manifests/pool.pp
@@ -71,8 +71,8 @@ define dhcp::pool (
   Optional[String[1]] $gateway              = undef,
   Optional[Variant[Array[String[1],1],String[1]]] $range = undef,
   Optional[String[1]] $failover             = undef,
-  Optional[String[1]] $options              = undef,
-  Optional[String[1]] $parameters           = undef,
+  Optional[Variant[Array[String[1]],String[1]]] $options          = undef,
+  Optional[Variant[Array[String[1]],String[1]]] $parameters       = undef,
   Optional[String[1]] $sharednetwork        = undef,
   Optional[Array[String]] $nameservers      = undef,
   Optional[Array[String]] $nameservers_ipv6 = undef,

manifests/pool6.pp
@@ -6,8 +6,8 @@ define dhcp::pool6 (
   Optional[Variant[Array[String[1],1],String[1]]] $range = undef,
   Optional[String[1]] $range_temp           = undef,
   Optional[String[1]] $failover             = undef,
-  Optional[String[1]] $options              = undef,
-  Optional[String[1]] $parameters           = undef,
+  Optional[Variant[Array[String[1]],String[1]]] $options          = undef,
+  Optional[Variant[Array[String[1]],String[1]]] $parameters       = undef,
   Optional[String[1]] $sharednetwork        = undef,
   Optional[Array[String]] $nameservers      = undef,
   Optional[Array[String]] $nameservers_ipv6 = undef,